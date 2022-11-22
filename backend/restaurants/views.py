42805bf60fc2d9a80852a204ade29a54d4eaa1c3
jongmee <ra123221@gmail.com>
2022-11-22 20:42:54 +0900

    category api

--- backend/restaurants/views.py
+++ backend/restaurants/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from .models import Restaurant
-from .serializers import RestaurantSerializer
+from .models import Restaurant, TypeCategory, LocationCategory
+from .serializers import RestaurantSerializer, TypeCategorySerializer, LocationCategorySerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView
 
 class RestaurantViewSet(ModelViewSet):
      queryset = Restaurant.objects.all()
      serializer_class = RestaurantSerializer
+
+class TypeCategoryViewSet(ListAPIView):
+     queryset = TypeCategory.objects.all()
+     serializer_class = TypeCategorySerializer
+
+class LocationCategoryViewSet(ListAPIView):
+     queryset =LocationCategory.objects.all()
+     serializer_class = LocationCategorySerializer
