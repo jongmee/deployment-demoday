e539f7820020e11f2543bb11f07052db8c9b7a32
jongmee <ra123221@gmail.com>
2022-11-24 17:23:00 +0900

    docker update

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -1,12 +1,17 @@
 from .common import *
 
-# DEBUG = os.environ.get("DEBUG") in ["1", "t", "true", "T", "True"]
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+DEBUG = os.environ.get("DEBUG") in ["1", "t", "true", "T", "True"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
 DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
 
-# AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"]
-# AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
-AZURE_ACCOUNT_NAME = 'demodaystorage'
-AZURE_ACCOUNT_KEY = 'kfycL+iOXqJvqAydRi96IgGK+/63FTAMEJEC3SId86tkjfzQJquuIMpJConp0A1JJSU31QutJEj8+ASt1G7agQ=='
+AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"]
+AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"level": "ERROR", "class": "logging.StreamHandler",},},
+    "loggers": {"django": {"handlers": ["console"], "level": "ERROR",},},
+}
